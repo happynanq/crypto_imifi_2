8d60a6bf119699070d60d1e75df84b0aa230e3e0
Kirill <54491606+happynanq@users.noreply.github.com>
2024-04-02 02:04:50 +0700

    Add files via upload

---
+++ get_bot.py
@@ -0,0 +1,11 @@
+from aiogram import Bot
+
+bot = ''
+def get_bot() -> Bot:
+    global bot
+    return bot
+
+def bot_def():
+    global bot
+    bot = Bot('7026029123:AAEfdoqqZNT9uchjiK4dQ1PbQPLqoorbAvo')
+#ПИТОН ХУЙНЯ
---
+++ helper/get_bot.py
@@ -0,0 +1,11 @@
+from aiogram import Bot
+
+bot = '6990640190:AAGuhlPtMP5q3BH2MPyTNdYUCEtabRwKsso'
+def get_bot() -> Bot:
+    global bot
+    return bot
+
+def bot_def():
+    global bot
+    bot = Bot('')
+#ПИТОН ХУЙНЯ
