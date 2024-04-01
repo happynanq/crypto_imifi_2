95eac7d15a516c2864f3efa6f0974ac203076895
Kirill <54491606+happynanq@users.noreply.github.com>
2024-04-02 02:28:10 +0700

    Update get_bot.py

--- helper/get_bot.py
+++ helper/get_bot.py
@@ -1,11 +1,11 @@
 from aiogram import Bot
 
-bot = '6990640190:AAGuhlPtMP5q3BH2MPyTNdYUCEtabRwKsso'
+bot = ''
 def get_bot() -> Bot:
     global bot
     return bot
 
 def bot_def():
     global bot
-    bot = Bot('')
+    bot = Bot('6990640190:AAGuhlPtMP5q3BH2MPyTNdYUCEtabRwKsso')
 #ПИТОН ХУЙНЯ
